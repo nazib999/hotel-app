752a43cb365a60d2cdc89c67c3a4d9363b017ab0
nazib ullah <najibullahnajib758@gmail.com>
2024-03-07 03:19:27 +0600

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
-import List from './pages/list/List'
-import Hotel from './pages/hotel/Hotel'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/hotels' element={<List/>}></Route>
-      <Route path='/hotels/:id' element={<Hotel/>}></Route>
-    </Routes>
+    <BrowserRouter basename="/hotel-app/">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/hotels" element={<List />}></Route>
+        <Route path="/hotels/:id" element={<Hotel />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
